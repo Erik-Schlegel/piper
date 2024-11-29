c322602366a69ef2f89840aff2b13359945ef5fc
Erik-Schlegel <epschlegel@gmail.com>
2024-11-29 07:08:34 +0000

    Support seamless indefinite loop, refactor audio effects.

--- player.py
+++ player.py
@@ -1,25 +1,33 @@
 import signal
 import sounddevice as sd
 from pysndfx import AudioEffectsChain
-
+import numpy as np
 
 def play_audio(track):
     try:
-        # Ignore signal handlers for the child process so we don't try to kill the proc more than once.
+        # Play calls are handled in a separate process. Ignore signal handlers
+        # internally, oherwise we get errors on exit.
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
 
-        samples = get_processed_samples(track)
-        if samples.ndim == 1:
-            channels = 1
-        elif samples.ndim == 2:
-            channels = samples.shape[1]
-        else:
-            raise ValueError("Invalid audio data shape")
+        samples = get_fx_processed_samples(track)
+        track_options = track[1].get('options', {})
 
-        with sd.OutputStream(samplerate=41000, channels=channels) as stream:
-            stream.write(samples)
+        sample_rate = 41000
+        fade_samples = int((track_options.get('crossFadeDuration', 0.25)) * sample_rate)
 
+        crossfade_samples = get_crossfade_samples(samples, fade_samples)
+
+        channel_count = 2 if samples.ndim == 2 else 1
+        with sd.OutputStream(samplerate=sample_rate, channels=channel_count) as stream:
+            while True:
+                if channel_count == 1:
+                    stream.write(samples[:-fade_samples])
+                else:
+                    stream.write(samples[:-fade_samples, :])
+
+                if (track_options.get('loop', False)):
+                    stream.write(crossfade_samples)
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
@@ -27,25 +35,35 @@ def play_audio(track):
         sd.stop()
 
 
-def get_processed_samples(track):
-    track_config = track[1]
-    volume = track_config['options'].get('volume', 1)
+def get_crossfade_samples(samples, fade_samples):
+    channel_count = 2 if samples.ndim == 2 else 1
 
-    fx = get_fx_chain(track_config)
-    return (fx(track[0] * volume).T)  # fx inverts shape of samples; '.T' transposes it back.
+    end_chunk = samples[-fade_samples:]
+    start_chunk = samples[:fade_samples]
+    fade_curve = np.linspace(0, 1, fade_samples, dtype=samples.dtype)
+
+    crossfade = None
+    if channel_count == 1:
+        crossfade = (end_chunk * (1 - fade_curve)).astype(samples.dtype) + \
+               (start_chunk * fade_curve).astype(samples.dtype)
+    else:
+        crossfade = (end_chunk * (1 - fade_curve)[:, np.newaxis]).astype(samples.dtype) + \
+               (start_chunk * fade_curve[:, np.newaxis]).astype(samples.dtype)
+
+    return crossfade
 
 
-def get_fx_chain(track_config):
+def get_fx_processed_samples(track):
+    track_options = track[1]
+    volume = track_options['options'].get('volume', 1)
 
     fx = AudioEffectsChain()
 
-    reverb_conf = track_config.get('options',{}).get('reverb', {})
+    reverb_conf = track_options.get('options',{}).get('reverb', {})
 
     if reverb_conf.get('reverberance', 0) > 0:
-        print(f'applying reverb to track: {track_config["path"]}')
         fx = fx.reverb(**reverb_conf)
-    else:
-        print(f'not applying reverb to track: {track_config["path"]}')
 
-    return fx
+    return (fx(track[0] * volume).T)  # fx inverts shape of samples; '.T' transposes it back.
+
 
